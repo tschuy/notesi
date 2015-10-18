cd85d0be8750833e88757111dbcc6eda41279f51
Evan Tschuy <evantschuy@gmail.com>
2015-10-18 08:24:54 -0700

    sdaf

--- proj/settings.py
+++ proj/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'proj.notesi.middleware'
 )
 
 ROOT_URLCONF = 'proj.urls'
